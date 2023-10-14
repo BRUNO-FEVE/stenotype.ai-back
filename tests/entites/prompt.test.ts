766aed4cd064bb681e33e4b7daec3a5649254a81
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-10-14 02:29:06 -0300

    feat: new prompt entity
    added the prompt entity with validation and tests

---
+++ tests/entites/prompt.test.ts
@@ -0,0 +1,34 @@
+import { expect, test } from "vitest";
+import { Prompt } from '../../src/entites/prompt'
+import { ObjectId } from "mongodb";
+
+test('create prompt', () => { 
+    const prompt = new Prompt({
+        id: new ObjectId(),
+        title: 'Title Test',
+        template: 'template Test',
+    })
+
+    expect(prompt).toBeInstanceOf(Prompt)
+    expect(prompt.title).toEqual('Title Test')
+})
+
+test('invalid title', () => {
+    expect(() => {
+        new Prompt({
+            id: new ObjectId(),
+            title: 'ab',
+            template: 'Template Test'
+        })
+    }).toThrowError()
+})
+
+test('invalid template', () => {
+    expect(() => {
+        new Prompt({
+            id: new ObjectId(),
+            title: 'Title Test',
+            template: 'ab'
+        })
+    }).toThrowError()
+})
