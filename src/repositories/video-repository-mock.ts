d1b7f336004e0c2a95bac3702f9e7c17e4bb41f6
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-10-17 08:30:10 -0300

    feat: video repository mock
    video repository mock to test the usecases

---
+++ src/repositories/video-repository-mock.ts
@@ -0,0 +1,19 @@
+import { Video } from "../entites/video";
+import { IVideosRepository } from "./i-videos-repository";
+
+export class videoRepositoryMock implements IVideosRepository {
+  public items: Video[] = [];
+
+  async findById(videoId: string): Promise<Video> {
+    const video = this.items.find((videoSearch) => videoSearch.id === videoId);
+    if (!video) return;
+    return video;
+  }
+
+  async create(video: Video): Promise<Video> {
+    const videoExistis = this.items.includes(video);
+    if (videoExistis) return;
+    this.items.push(video);
+    return video;
+  }
+}
